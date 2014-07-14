8c1f553ba4c646a7df358fb3a3b23215d5fe67ab
Brett Bukowski <brett.bukowski@gmail.com>
2014-07-14 06:54:25 -0600

    Swift language changes for arrays

--- arrays.swift
+++ arrays.swift
@@ -5,10 +5,10 @@ var strings = ["a", "b", "c"]
 var strings2: String[] = ["d", "e", "f"]
 
 // Declare an empty array.
-var strings3 = String[]()
+var strings3 = [String]()
 
 // Fill an array.
-var strings4 = String[](count: 3, repeatedValue: "hey")
+var strings4 = [String](count: 3, repeatedValue: "hey")
 
 
 // Arrays must contain values of a single type.
@@ -35,10 +35,10 @@ strings[0] = "a"
 
 // # Slices
 
-strings[0..1] = ["a"]                       // basically the same as the previous assignment
-strings[0...1] = ["a", "b"]
-strings[0...3]                              // ["a", "b", "c", "d"]
-strings[0..strings.endIndex]                // ["a", "b", "c", "d", "e", "f", "g"]
+strings[0..<1] = ["a"]                       // Exclusive (basically the same as the prev assignment)
+strings[0...1] = ["a", "b"]                  // Inclusive
+strings[0...3]                               // ["a", "b", "c", "d"]
+strings[0..<strings.endIndex]                // ["a", "b", "c", "d", "e", "f", "g"]
 
 // # Methods
 
@@ -52,7 +52,7 @@ println(strings.removeAtIndex(0))          // a
 strings.map({
     (str: String) -> String in
     return str + "0"
-})                                         // ["a0", "b0", "c0", "d0", "e0", "f0", "g0"]
+    })                                         // ["a0", "b0", "c0", "d0", "e0", "f0", "g0"]
 strings.removeLast()
 
 // # Clearing
@@ -62,6 +62,6 @@ strings = []
 // # Using a loop to create a multidimensional array
 var rows = 10, cols = 10
 var dimensional = Array<Array<Int>>()
-for col in 0..10 {
+for col in 0..<10 {
     dimensional.append(Array(count: rows, repeatedValue:Int()))
 }
